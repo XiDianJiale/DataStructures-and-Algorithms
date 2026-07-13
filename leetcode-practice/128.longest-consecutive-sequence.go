2b606b7fb529c6cfe2ce4dfcf9544e9beb88f9c0
Jiale Hao <150508200+XiDianJiale@users.noreply.github.com>
2026-07-13 10:23:06 +0800

    Update 128.longest-consecutive-sequence.go

--- leetcode-practice/128.longest-consecutive-sequence.go
+++ leetcode-practice/128.longest-consecutive-sequence.go
@@ -2,3 +2,32 @@
 
 
 
+
+func longestConsecutive(nums []int) int {
+	m := make(map[int]struct{})
+	for _, v := range nums {
+		m[v] = struct{}{}
+	}
+
+	maxLen := 0
+	for k, _ := range m {
+		if _, ok := m[k-1]; ok {
+			continue
+		}
+		current := k
+		length := 1
+		for {
+			if _, ok := m[current+1]; !ok {
+				break
+			}
+			length++
+			current++
+		}
+		if length > maxLen {
+			maxLen = length
+		}
+
+	}
+	return maxLen
+
+}
